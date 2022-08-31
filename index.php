2568537b4262cfc3b59df0834da85a3d4bffc1d0
Yuriy Tigunov <192215@gmail.com>
2022-08-31 15:22:17 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo  '<h1>Hello from Kubernetes </h1> <br>';
+echo '<h2> Server IP: ' . $ip_server . '</h2>' . '<br>';
+echo 'Made by tigunov-it';
+echo 'Version: 2';
